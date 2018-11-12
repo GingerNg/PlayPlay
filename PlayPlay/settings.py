8aa125ea565b95fc82a3fb52d242f97d05213fa1
ginger <jinjie.wu@fir.ai>
2018-11-12 15:50:08 +0800

    @init

--- PlayPlay/settings.py
+++ PlayPlay/settings.py
@@ -127,7 +127,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -140,3 +141,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
